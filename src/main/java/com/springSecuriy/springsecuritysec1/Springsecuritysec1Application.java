b4753fd5dd15a3903e495be4fb1a378478dff267
siddhantpurohit216 <siddhantpurohit216@gmail.com>
2024-09-30 15:17:32 +0530

    initial security config

---
+++ src/main/java/com/springSecuriy/springsecuritysec1/Springsecuritysec1Application.java
@@ -0,0 +1,13 @@
+package com.springSecuriy.springsecuritysec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecuritysec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecuritysec1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springSecuriy/springsecuritysec1/Springsecuritysec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecuriy.springsecuritysec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecuritysec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
