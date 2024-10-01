729afd85fe661737a86824708f27f119b1f5ac80
siddhantpurohit216 <siddhantpurohit216@gmail.com>
2024-10-01 12:03:06 +0530

    comp Sec-3, bycrpt,passwordEconder,compromisedPasswordChecker

--- src/main/java/com/springSecuriy/springsecuritysec1/Configs/ProjectSecurityConfig.java
+++ src/main/java/com/springSecuriy/springsecuritysec1/Configs/ProjectSecurityConfig.java
@@ -2,8 +2,17 @@ package com.springSecuriy.springsecuritysec1.Configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
@@ -15,9 +24,30 @@ public class ProjectSecurityConfig {
 
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact").permitAll());
-        http.formLogin(dfl->dfl.disable());
-        http.httpBasic(dhb->dhb.disable());
+                .requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user= User.withUsername("user").password("{noop}1234").authorities("read").build();
+        UserDetails admin= User.withUsername("admin").password("{bcrypt}$2a$12$w9LeZfI77qosoM24ja882eO6l/Ia2vrRHOLNvVSjWOwfFa.I60dEi").authorities("read").build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker()
+    {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
